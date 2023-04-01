b2fe522f9568b109f012ab4b7ae3af508b28a900
Jake Soenneker <jake@soenneker.com>
2023-03-31 20:15:22 -0500

    Initial commit

---
+++ src/IntExtension.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace Soenneker.Extensions.Int;
+
+/// <summary>
+/// A collection of useful int (Int32) extension methods
+/// </summary>
+public static class IntExtension
+{
+    /// <summary>
+    /// Formats integers so they have commas every three decimal places. Shorthand for "N0"
+    /// </summary>
+    [Pure]
+    public static string ToDisplay(this int value)
+    {
+        return value.ToString("N0");
+    }
+
+    /// <summary>
+    /// Formats integers so they have commas every three decimal places. Shorthand for "N0".
+    /// </summary>
+    /// <returns>Null if value is null, unless dashIfNull is true</returns>
+    [Pure]
+    public static string? ToDisplay(this int? value, bool dashIfNull = false)
+    {
+        if (value == null)
+        {
+            if (dashIfNull)
+                return "-";
+        }
+
+        return value?.ToString("N0");
+    }
+}
