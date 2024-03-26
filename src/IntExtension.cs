4869ad44db604dfced97ff3a564167c74624a3ce
Jake Soenneker <jake@soenneker.com>
2024-03-25 20:00:08 -0500

    Addt method

--- src/IntExtension.cs
+++ src/IntExtension.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.Contracts;
+﻿using System;
+using System.Diagnostics.Contracts;
 
 namespace Soenneker.Extensions.Int;
 
@@ -47,5 +48,29 @@ public static class IntExtension
     {
         var result = (char)(value < 10 ? value + '0' : value - 10 + 'A');
         return result;
+    }
+
+    /// <summary>
+    /// Converts a Unix timestamp to a <see cref="DateTime"/> in UTC. (Also available as a <see cref="long"/> extension)
+    /// </summary>
+    /// <remarks>
+    /// This method extends the <see cref="int"/> type, allowing a Unix timestamp
+    /// (which counts the number of seconds since the Unix Epoch at 00:00:00 UTC on 1 January 1970)
+    /// to be directly converted into a <see cref="DateTime"/> object.
+    /// The result is a UTC <see cref="DateTime"/> representing the same moment in time as the Unix timestamp.
+    /// </remarks>
+    /// <param name="unixTime">The Unix timestamp to convert, represented as the number of seconds since the Unix Epoch.</param>
+    /// <returns>A UTC <see cref="DateTime"/> object representing the same moment in time as the specified Unix timestamp.</returns>
+    /// <example>
+    /// This example shows how to convert a Unix timestamp to a <see cref="DateTime"/>:
+    /// <code>
+    /// long unixTimestamp = 1588305600;
+    /// DateTime dateTime = unixTimestamp.ToDateTimeFromUnixTime();
+    /// Console.WriteLine(dateTime); // Output: 5/1/2020 12:00:00 AM (Depending on the system's time zone, output might vary)
+    /// </code>
+    /// </example>
+    public static DateTime ToDateTimeFromUnixTime(this int unixTime)
+    {
+        return DateTimeOffset.FromUnixTimeSeconds(unixTime).UtcDateTime;
     }
 }
